4dd61be0609018111a3f307e9538aeffbfdf894f
Rimuru41 <dangghorahi944@gmail.com>
2026-03-19 22:54:42 +0545

    session

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.set('views', cfg.dir.views);
 
 // Session middleware
 app.use(session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || whatever,
     resave: false,
     saveUninitialized: false,
     cookie: {
